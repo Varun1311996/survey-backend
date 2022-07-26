b82daff0c8a3a27f6d3717d308da9875a47da326
lbc7 <82112854+lbc7@users.noreply.github.com>
2022-07-26 14:04:09 -0500

    Add files via upload

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -45,14 +45,14 @@ isModerator = (req, res, next) => {
   User.findByPk(req.userId).then(user => {
     user.getRoles().then(roles => {
       for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "guest") {
+        if (roles[i].name === "user") {
           next();
           return;
         }
       }
 
       res.status(403).send({
-        message: "Require Guest Role!"
+        message: "Require User Role!"
       });
     });
   });
@@ -62,7 +62,7 @@ isModeratorOrAdmin = (req, res, next) => {
   User.findByPk(req.userId).then(user => {
     user.getRoles().then(roles => {
       for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "guest") {
+        if (roles[i].name === "user") {
           next();
           return;
         }
